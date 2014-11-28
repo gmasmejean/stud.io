4bfbcd20099d974f09c5c41a6ab9488990787599
gma <gmasmejean@thestudnet.com>
2014-11-28 11:05:38 +0100

    check merge nma

--- test/test.js
+++ test/test.js
@@ -1,7 +1,5 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-
+// Session storage adapter tests.
+require('./memcached_adapter');
+require('./redis_adapter');
+require('./memory_adapter');
